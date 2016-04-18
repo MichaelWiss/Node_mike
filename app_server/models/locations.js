89f20c904caebe72ad5d0119344427f8bd6d92a1
Michael Wiss <michael.wiss@gmail.com>
2016-04-18 18:50:44 -0500

    locations schema

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,2 +1,8 @@
 var mongoose = require( 'mongoose' );
-var locationSchema = mongoose.Schema({ });
+var locationSchema = mongoose.Schema({ 
+  name: 'Starcups',
+  address: '125 High Street, Reading, RG6 1PS',
+  rating: {type: Number, "default": 0}
+  facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+  distance: '100m'
+});
