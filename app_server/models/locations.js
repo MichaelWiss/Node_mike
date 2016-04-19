d031d803e6369fcf6e3e963d194906d715e0e15c
Michael Wiss <michael.wiss@gmail.com>
2016-04-19 16:06:16 -0500

    scema validations

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,6 +1,6 @@
 var mongoose = require( 'mongoose' );
 var locationSchema = mongoose.Schema({ 
-  name: 'Starcups',
+  name: {type: String, required: true}
   address: '125 High Street, Reading, RG6 1PS',
   rating: {type: Number, "default": 0}
   facilities: ['Hot drinks', 'Food', 'Premium wifi'],
