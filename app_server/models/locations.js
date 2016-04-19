667d331fcb5bff331fa6a55cbf24e383ac74e963
Michael Wiss <michael.wiss@gmail.com>
2016-04-19 17:02:00 -0500

    openingTimes variable

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,8 +1,17 @@
 var mongoose = require( 'mongoose' );
+
+var openingTimeSchema = mongoose.Schema({
+	days: {type: String, required: true},
+	opening: String,
+	closing: String,
+	closed: {type: Boolean, required: true}
+});
+
 var locationSchema = mongoose.Schema({ 
   name: {type: String, required: true},
   address: String,
   rating: {type: Number, "default": 0, min: 0, max: 5},
   facilities: [String],
-  distance: {type: [Number], index: '2dsphere'}
+  coords: {type: [Number], index: '2dsphere'},
+  openingTimes: [openingTimesSchema]
 });
