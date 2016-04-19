5ed883e913ca423e202c480f76c570db10f4e001
Michael Wiss <michael.wiss@gmail.com>
2016-04-19 16:28:19 -0500

    datatypes for locationschema

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,8 +1,8 @@
 var mongoose = require( 'mongoose' );
 var locationSchema = mongoose.Schema({ 
   name: {type: String, required: true}
-  address: '125 High Street, Reading, RG6 1PS',
-  rating: {type: Number, "default": 0}
-  facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-  distance: '100m'
+  address: String,
+  rating: {type: Number, "default": 0, min: 0, max: 5}
+  facilities: [String],
+  distance: {type: [Number], index: '2dsphere'}
 });
