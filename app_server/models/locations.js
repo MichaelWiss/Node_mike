fa08bd141c330054beca14b813e4c86d1e5c45b5
Michael Wiss <michael.wiss@gmail.com>
2016-04-19 17:30:11 -0500

    reviewSchema added

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -7,11 +7,20 @@ var openingTimeSchema = mongoose.Schema({
 	closed: {type: Boolean, required: true}
 });
 
+var reviewSchema = new mongoose.Schema({
+	author: String,
+	rating: {type: Number, required: true, min: 0 max: 5},
+	reviewText: String,
+	createdOn: {type: Date, "default", Date.now}
+});
+
 var locationSchema = mongoose.Schema({ 
   name: {type: String, required: true},
   address: String,
   rating: {type: Number, "default": 0, min: 0, max: 5},
   facilities: [String],
   coords: {type: [Number], index: '2dsphere'},
-  openingTimes: [openingTimesSchema]
+  openingTimes: [openingTimesSchema],
+  reviews: [reviewSchema]
 });
+
