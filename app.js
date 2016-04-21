112355f4c812e960dc68e37487ff0af099a0bedf
Michael Wiss <michael.wiss@gmail.com>
2016-04-21 16:56:29 -0500

    add app_server routes

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/api', routesApi);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
