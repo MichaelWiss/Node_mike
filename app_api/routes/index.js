865776e0be89e948b219ee61bf58efc3206767b8
Michael Wiss <michael.wiss@gmail.com>
2016-04-22 13:51:25 -0500

    locations routes

---
+++ app_api/routes/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlReviews = require('../controllers/reviews');
+
+// locations
+router.get()
