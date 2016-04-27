95d665bc319118899435cdf1189b1fefa2d5e27b
Michael Wiss <michael.wiss@gmail.com>
2016-04-27 16:27:58 -0500

    create a subdocument by locationid

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -8,7 +8,25 @@ var sendJSONresponse = function(res, status, content) {
 };
 
 module.exports.reviewsCreate = function (req, res) {
-    sendJSONresponse(res, 200, {"status" : "success"});
+    var locationid = req.params.locationid;
+    if(locationid) {
+    	Loc
+    	   .findById(locationid)
+    	   .select('review')
+    	   .exec(
+    	   	function(err, location) {
+    	   		if (err, location) {
+    	   			sendJSONresponse(res, 400, err);
+    	   		} else {
+    	   			doAddReview(req, res, location);
+    	   		}
+    	   	}
+    );
+   } else {
+   	sendJSONresponse(res, 404, {
+   		"message": "Not found, locationid required"
+   	});
+   }
  };
 
  module.exports.reviewsReadOne = function (req, res) {
