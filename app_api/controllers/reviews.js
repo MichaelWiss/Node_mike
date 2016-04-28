d801508a0793fba94af8e8e3046c05b9412332d2
Michael Wiss <michael.wiss@gmail.com>
2016-04-27 20:22:48 -0500

    delete review

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -156,9 +156,40 @@ module.exports.reviewsCreate = function (req, res) {
      .exec(
      	function(err, location) {
      		if (!location) {
-     			
+     		sendJSONresponse(res, 404, {
+     			"message": "locationid not found"
+     		});
+     		return;
+     		} else if (err) {
+     			sendJSONresponse(res, 400, err);
+     			return;
      		}
-     	})
+     		if (location.reviews && location.reviews.length > 0) {
+     			if (!location.reviews.id(req.params.reviewid)) {
+     				sendJSONresponse(res, 404, {
+     					"message": "reviewid not found"
+     				});
+     			} else {
+                  location.reviews.id(req.params.reviewid).remove();
+                  location.save(function(err) {
+                  	if (err) {
+                  		sendJSONresponse(res, 404, err);
+                  	} else {
+                  		updateAverageRating(location._id);
+                  		sendJSONresponse(res, 204, null); 
+                  	}
+                  });
+              }
+          } else {
+          	sendJSONresponse(res, 404, {
+                "message": "No review to delete"
+                  		});
+              }
+            }
+        );
+    };
+     	
+ 
 
 
 
