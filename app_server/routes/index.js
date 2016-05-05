53a2823e9474ae84894bcdd7e2f442e1a556ddb3
Michael Wiss <michael.wiss@gmail.com>
2016-05-04 20:38:42 -0500

    mongo bug with location object?

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,23 +3,13 @@ var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-/* Locations */
+/* Locations pages */
 router.get('/', ctrlLocations.homelist);
-// router.get('/location', ctrlLocations.locationInfo);
-// router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/:locationid', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
 
-
-/*var ctrlMain = require('../controllers/main');
-
-var homepageController = function (req, res) {
-	res.render('index', {title: 'Express' });
-}; */
-
-/* GET home page. 
-router.get('/', ctrlMain.index); 
-*/
 
 module.exports = router;
