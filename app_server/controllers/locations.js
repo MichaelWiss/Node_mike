44bd84c0099876073dd47bda809bba584cae9860
Michael Wiss <michael.wiss@gmail.com>
2016-05-10 14:25:07 -0500

    _showError variable

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -10,7 +10,7 @@ var _isNumeric = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-var _formatDistance = function (distance) {
+var _formatDistance = function(distance) {
     var numDistance, unit;
     if (distance && _isNumeric(distance)) {
      if (distance > 1) {
@@ -25,6 +25,25 @@ var _formatDistance = function (distance) {
     return "?";
   }
  };
+
+ var _showError = function (req, res, status) {
+  var title, content;
+  if (status === 404) {
+    title = "404, page not found";
+    content = "Oh dear. Looks like we can't find this page. Sorry.";
+  } else if (status === 500) {
+    title = "500, internal server error";
+    content = "How embarrassing. There's a problem with our server.";
+  } else {
+    title = status + ", something's gone wrong";
+    content = "Something, somewhere, has gone just a little bit wrong.";
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title : title,
+    content : content
+  });
+};
 
 
 var renderHomepage = function(req, res, responseBody){
