b6b032fabda03ddbc3e32ef7e3e8538cb5514816
Michael Wiss <michael.wiss@gmail.com>
2016-05-16 14:21:19 -0500

    ohter.js

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET home page */
 module.exports.about = function(req, res) {
-	res.render('generic-text', { title: 'About' });
+	res.render('generic-text', {
+	    title: 'About Loc8r',
+	    
+  });
 };
