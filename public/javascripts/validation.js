fd0142791b95e0522cbe785a3b255f28b91d95e2
Michael Wiss <michael.wiss@gmail.com>
2016-05-31 18:02:34 -0500

    jquery

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,5 +1,10 @@
 $('#addReview').submit(function (e) {
 	$('.alert.alert-danger').hide();
 	if (!$('input#name').val() || !$('select#rating').val() ||
-		!$)
-})
+		!$('textarea#review').val()) {
+	if ($('.alert.alert-danger').length) {
+		$('.alert.alert-danger').show();
+	} else {
+	  $(this).prepend('<div')
+	}
+	}
