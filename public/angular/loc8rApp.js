e72a2c623bbb65667d4fd2bb416284836d266aa4
Michael Wiss <michael.wiss@gmail.com>
2016-06-01 18:56:01 -0500

    hardcode locations in loc8rApp.js

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -1,3 +1,16 @@
 angular.module('loc8rApp', []);
 
-var locationListCtrl = function ($scope) {}
+var locationListCtrl = function ($scope) {
+	$scope.data = {
+	  locations: [{
+	  	name : 'Burger Queen',
+	  	address : '125 High Street, Reading, RG6 1PS',
+	  	rating : 3,
+	  	facilities : ['Hot drinks', 'Food', 'Premium wifi', ],
+	  	distance : '0.296456',
+	  	_id: '5370a35f2536f6785f8dfb6a'
+	  }, {
+	  	name
+	  }]
+	}
+}
