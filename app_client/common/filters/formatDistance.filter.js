f2532db4a77f6011c9544f516d25a9e7ea545078
Michael Wiss <michael.wiss@gmail.com>
2016-06-17 17:46:42 -0500

    fixed parseFloat bug

--- app_client/common/filters/formatDistance.filter.js
+++ app_client/common/filters/formatDistance.filter.js
@@ -1,18 +1,18 @@
 (function () {
-	
+
 angular 
   .module('loc8rApp')
   .filter('formatDistance', formatDistance);
 
 var _isNumeric = function (n) {
-	return !isNaN(parsefloat(n)) && isFinite(n);
+	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 function formatDistance () {
 	return function (distance) {
 		var numDistance, unit;
 		if (distance && _isNumeric(distance)) {
-		 if (distance >1) {
+		 if (distance > 1) {
 			numDistance = parseFloat(distance).toFixed(1);
 			unit = 'km';
 		  } else {
