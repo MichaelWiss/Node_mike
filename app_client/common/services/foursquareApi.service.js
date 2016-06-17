9d41165232c0b2a2971b2563491e56264adaad12
Michael Wiss <michael.wiss@gmail.com>
2016-06-17 14:19:58 -0500

    added loc8rData and geolocation services to homeController

---
+++ app_client/common/services/foursquareApi.service.js
@@ -0,0 +1,3 @@
+angular
+   .module('loc8rApp')
+   .service('foursquareApi', foursquareApi);
--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -2,7 +2,7 @@ angular
   .module('loc8rApp')
   .controller('homeCtrl', homeCtrl);
 
-function homeCtrl ($scope) {
+function homeCtrl (loc8rData, geolocation) {
 	var vm = this;
 	vm.pagHeader = {
 		title: 'Loc8r',
@@ -11,4 +11,25 @@ function homeCtrl ($scope) {
 	vm.sidebar = {
 		content: "Looking for wifi and a seat etc etc"
 	};
+	vm.message = "Checking your location";
+	vm.getData = function (position) {
+		var lat = position.coords.latitude,
+		    lng = position.coords.longitude;
+		vm.message = "Searching for nearby places";
+		loc8rData.locationByCoords(lat, lng)
+		   .success(function(data) {
+		   	vm.message = data.length > 0 ? "" : "No locations found nearby";
+		   	vm.data = { locations: data };
+		   })
+		   .error(function (e) {
+		   	vm.message = "Sorry, something's gone wrong";
+		   });
+		};
+
+		vm.noGeo = function () {
+			vm.$apply(function() {
+				vm.message = "Geolocation is not supported by this browser.";
+			});
+		};
+		geolocation.getPositon(vm.getData, vm.showError, vm.noGeo);
 }
