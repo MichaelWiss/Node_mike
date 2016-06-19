49dcb50ef6e18a2b682f106b54ee369c2db3d3b9
Michael Wiss <michael.wiss@gmail.com>
2016-06-18 19:57:55 -0500

    clean up

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -3,7 +3,6 @@
 angular
   .module('loc8rApp')
   .controller('homeCtrl', homeCtrl);
-homeCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
 
 function homeCtrl ($scope, loc8rData, geolocation) {
 	var vm = this;
