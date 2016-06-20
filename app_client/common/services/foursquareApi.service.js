243772fe30aa2c1ebe643285cc7f63f9a4d248ec
Michael Wiss <michael.wiss@gmail.com>
2016-06-20 10:43:17 -0500

    foursquare api buildout

--- app_client/common/services/foursquareApi.service.js
+++ app_client/common/services/foursquareApi.service.js
@@ -4,8 +4,24 @@
 angular
    .module('loc8rApp')
    .service('foursquareApi', foursquareApi);
+
+var foursquareConfig = {
+	'secrets' : {
+		'clientId' : 'APP_CLIENT_ID',
+		'clientSecret' : 'APP_CLIENT_SECRET',
+		'redirectUrl' : 'REDIRECT_URL'
+	}
+};
 function foursquare (geolocation) {
- var foursquare = require('node-foursquare-venues')('clientId', 'secretId');
+ var foursquare = require('node-foursquare-venues')(foursquareConfig);
+
+ app.get('/', function (req, res) {
+
+
+
+
+
+});
 
 
 
