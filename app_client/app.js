6cce5591b06dbe4f6aea843fd3ecc72dcb4ebc11
Michael Wiss <michael.wiss@gmail.com>
2016-06-28 12:48:28 -0500

    restart

--- app_client/app.js
+++ app_client/app.js
@@ -3,9 +3,9 @@ angular.module('loc8rApp', ['ngRoute']);
 function config ($routeProvider) {
 	$routeProvider
 	   .when('/', {
-         templateUrl: 'places/placesResults.html',
-         controller: 'placesExplorerController.js',
-         controllerAs: 'vm'
+	   	 templateUrl: 'home/home.view.html',
+	   	 controller: 'homeCtrl',
+	   	 controllerAs: 'vm'
 	   })
 	   .otherwise({redirectTo: '/'});
 }
