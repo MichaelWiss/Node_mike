2c66b201042d6260d8b67541ab97710832fe95ed
Michael Wiss <michael.wiss@gmail.com>
2016-07-02 14:59:52 -0500

    foursquare api, browserify

--- app_client/app.js
+++ app_client/app.js
@@ -20,28 +20,4 @@ angular
    .config(['$routeProvider', config]);
 })();
 
-(function () {
-angular
-   .module('loc8rApp')
-   .service('foursquareApi', foursquareApi);
 
-function foursquareApi ($http) {
-   var foursquareApi = angular.module('foursquarevenues', []);
-   var foursquare = (require('foursquarevenues'))('env.clientId', 'env.clientSecret');
-   var params = {
-    "ll": "40.7,-74"
-  };
-
-   foursquare.getVenues(params, function(error, venues) {
-    if (!error) {
-      console.log(venues);
-    }
-  });
- 
-  foursquare.exploreVenues(params, function(error, venues) {
-    if (!error) {
-        console.log(venues);
-  }
-});
-}
-})();
