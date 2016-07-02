b41ae32dc932f51ff13ad7c215aee811c4c422d9
Michael Wiss <michael.wiss@gmail.com>
2016-07-02 13:49:44 -0500

    installed requirejs

--- app_client/app.js
+++ app_client/app.js
@@ -12,7 +12,36 @@ function config ($routeProvider) {
 }
 
 
+
+
+
 angular
    .module('loc8rApp')
    .config(['$routeProvider', config]);
+})();
+
+(function () {
+angular
+   .module('loc8rApp')
+   .service('foursquareApi', foursquareApi);
+
+function foursquareApi ($http) {
+   var foursquareApi = angular.module('foursquarevenues', []);
+   var foursquare = (require('foursquarevenues'))('env.clientId', 'env.clientSecret');
+   var params = {
+    "ll": "40.7,-74"
+  };
+
+   foursquare.getVenues(params, function(error, venues) {
+    if (!error) {
+      console.log(venues);
+    }
+  });
+ 
+  foursquare.exploreVenues(params, function(error, venues) {
+    if (!error) {
+        console.log(venues);
+  }
+});
+}
 })();
