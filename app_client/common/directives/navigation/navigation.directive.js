8b9808c37caadf2173bd526d312bc149b8fcb204
Michael Wiss <michael.wiss@gmail.com>
2016-07-18 13:50:31 -0500

    navigation.directive.js added

---
+++ app_client/common/directives/navigation/navigation.directive.js
@@ -0,0 +1,13 @@
+(function () {
+	
+	angular
+	  .module('loc8rApp')
+	  .directive('navigation', navigation);
+
+	function navigation () {
+	 return {
+	    restrict: 'EA',
+	    templatUrl: '/common/directives/navigation/navigation.template.html'
+	 };
+	}
+}) ();
