f430ef169a02d38e05226b6e04f6e92d3a3b3135
Michael Wiss <michael.wiss@gmail.com>
2016-07-21 10:49:10 -0500

    module loading bugs

--- app_client/app.js
+++ app_client/app.js
@@ -1,30 +1,27 @@
 (function () {
+
 angular.module('loc8rApp', ['ngRoute', 'ngSanitize']);
 
-function config ($routeProvider, $locationProvider) {
-	$routeProvider
-	   .when('/', {
-	   	 templateUrl: 'home/home.view.html',
-	   	 controller: 'homeCtrl',
-	   	 controllerAs: 'vm'
-	   })
-	   .when('/about', {
-	   	 templateUrl: '/common/views/genericText.view.html',
-	   	 controller: 'aboutCtrl',
-	   	 controllerAs: 'vm'
-	   })
-	   .otherwise({redirectTo: '/'});
+   function config ($routeProvider, $locationProvider) {
+	  $routeProvider
+	     .when('/', {
+	   	   templateUrl: 'home/home.view.html',
+	   	   controller: 'homeCtrl',
+	   	   controllerAs: 'vm'
+	     })
+	     .when('/about', {
+	   	   templateUrl: '/common/views/genericText.view.html',
+	   	   controller: 'aboutCtrl',
+	   	   controllerAs: 'vm'
+	     })
+	     .otherwise({redirectTo: '/'});
 
-	$locationProvider.html5Mode({enabled:true, requireBase: false});
-}
+	  $locationProvider.html5Mode({enabled:true, requireBase: false});
+    }
 
-
-
-
-
-angular
-   .module('loc8rApp')
-   .config(['$routeProvider', '$locationProvider', config]);
+    angular
+      .module('loc8rApp')
+      .config(['$routeProvider', '$locationProvider', config]);
 })();
 
 
