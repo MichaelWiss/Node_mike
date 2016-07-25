bd7204e696048ee302b49212e40fd5b8edc33659
Michael Wiss <michael.wiss@gmail.com>
2016-07-25 17:02:29 -0500

    locationById method added

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -3,10 +3,23 @@
 	angular
 	  .module('loc8rApp')
 	  .controller('locationDetailCtrl', locationDetailCtrl);
-    locationDetailCtrl.$inject = ['$routeParams'];
-	function locationDetailCtrl ($routeparams) {
+    locationDetailCtrl.$inject = ['$routeParams', 'loc8rData'];
+	function locationDetailCtrl ($routeparams, loc8rData) {
 	  var vm = this;
       vm.locationid = $routeParams.locationid;
+
+      loc8rData.locationById(vm.locationid)
+        .success(function(data) {
+        	vm.data = { location: data };
+            vm.pageHeader = {
+            	title: vm.data.location.name
+            };
+        })
+        .error(function (err) {
+        	concole.log(err);
+        });
+
+
 	  vm.pageHeader = {
 	     title: vm.locationid
 	  };
