b08d987dbfa09bef06c5ba68194f0b6396522f93
Michael Wiss <michael.wiss@gmail.com>
2016-07-30 14:06:11 -0500

    rating-stars dirctive

--- app_client/common/directives/ratingStars/ratingStars.directive.js
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -1,19 +1,37 @@
 (function () {
 
 
-angular
-   .module('loc8rApp')
-   .directive('ratingStars', ratingStars);
+// angular
+//    .module('loc8rApp')
+//    .directive('ratingStars', ratingStars);
 
-function ratingStars () {
-	return {
-		restrict: 'EA',
-		scope: {
-			thisRating : '=rating'
-		},
-		templateUrl: '/common/directives/ratingStars/ratingStars.template.html',
-    };
- }
+// function ratingStars () {
+// 	return {
+// 		restrict: 'EA',
+// 		scope: {
+// 			thisRating : '=rating'
+// 		},
+// 		templateUrl: '/common/directives/ratingStars/ratingStars.template.html',
+//     };
+//  }
+
+// })();
+
+
+angular
+      .module('loc8rApp')
+      .directive('ratingStars', function ($scope) {
+
+
+  $scope.ratingStates = [
+    {stateOn: 'fa-check-circle', stateOff: 'fa-check-circle-o'},
+    {stateOn: 'fa-star', stateOff: 'fa-start-o'},
+    {stateOn: 'fa-heart', stateOff: 'fa-ban'},
+    {stateOn: 'fa-heart'},
+    {stateOff: 'fa-power-off'}
+  ];
+});
 
 })();
+
 
