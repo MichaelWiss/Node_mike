9b9835511d0deb255f60a4f9e5c7b44cb3d83192
Michael Wiss <michael.wiss@gmail.com>
2016-07-31 13:23:49 -0500

    bug

--- app_client/common/directives/ratingStars/ratingStars.directive.js
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -1,37 +1,37 @@
 (function () {
 
 
-// angular
-//    .module('loc8rApp')
-//    .directive('ratingStars', ratingStars);
-
-// function ratingStars () {
-// 	return {
-// 		restrict: 'EA',
-// 		scope: {
-// 			thisRating : '=rating'
-// 		},
-// 		templateUrl: '/common/directives/ratingStars/ratingStars.template.html',
-//     };
-//  }
-
-// })();
-
-
 angular
-      .module('loc8rApp')
-      .directive('ratingStars', function ($scope) {
+   .module('loc8rApp')
+   .directive('ratingStars', ratingStars);
 
-
-  $scope.ratingStates = [
-    {stateOn: 'fa-check-circle', stateOff: 'fa-check-circle-o'},
-    {stateOn: 'fa-star', stateOff: 'fa-start-o'},
-    {stateOn: 'fa-heart', stateOff: 'fa-ban'},
-    {stateOn: 'fa-heart'},
-    {stateOff: 'fa-power-off'}
-  ];
-});
+function ratingStars () {
+	return {
+		restrict: 'EA',
+		scope: {
+			thisRating : '=rating'
+		},
+		templateUrl: '/common/directives/ratingStars/ratingStars.template.html',
+    };
+ }
 
 })();
+
+
+// angular
+//       .module('loc8rApp')
+//       .directive('ratingStars', function ($scope) {
+
+
+//   $scope.ratingStates = [
+//     {stateOn: 'fi-star', stateOff: 'fa-check-circle-o'},
+//     {stateOn: 'fi-star', stateOff: 'fa-start-o'},
+//     {stateOn: 'fa-heart', stateOff: 'fa-ban'},
+//     {stateOn: 'fa-heart'},
+//     {stateOff: 'fa-power-off'}
+//   ];
+// });
+
+// })();
 
 
