f82251f693027f7601a3cb0c9a6b55d5c064f1a8
Michael Wiss <michael.wiss@gmail.com>
2016-08-03 20:26:54 -0500

    foundation in directive

--- app_client/common/directives/ratingStars/ratingStars.directive.js
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -18,25 +18,25 @@ function ratingStars () {
 })();
 
 
-// angular.module('loc8rApp'),
-//  function ($scope) {
-//   $scope.rate = 7;
-//   $scope.max = 10;
-//   $scope.isReadonly = false;
+angular.module('loc8rApp'),
+ function ($scope) {
+  $scope.rate = 7;
+  $scope.max = 10;
+  $scope.isReadonly = false;
 
-//   $scope.hoveringOver = function(value) {
-//     $scope.overStar = value;
-//     $scope.percent = 100 * (value / $scope.max);
-//   };
+  $scope.hoveringOver = function(value) {
+    $scope.overStar = value;
+    $scope.percent = 100 * (value / $scope.max);
+  };
 
-//   $scope.ratingStates = [
-//     {stateOn: 'fi-star', stateOff: 'fa-check-circle-o'},
-//     {stateOn: 'fa-star', stateOff: 'fa-start-o'},
-//     {stateOn: 'fi-star', stateOff: 'fa-ban'},
-//     {stateOn: 'fi-star'},
-//     {stateOff: 'fi-star'}
-//   ];
-// });
+  $scope.ratingStates = [
+    {stateOn: 'fi-star', stateOff: 'fa-check-circle-o'},
+    {stateOn: 'fa-star', stateOff: 'fa-start-o'},
+    {stateOn: 'fi-star', stateOff: 'fa-ban'},
+    {stateOn: 'fi-star'},
+    {stateOff: 'fi-star'}
+  ];
+});
 
 
-// })();
+})();
