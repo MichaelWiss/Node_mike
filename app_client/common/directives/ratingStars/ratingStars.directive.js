f43f946bda357708c7c140574a11290a9252bee5
Michael Wiss <michael.wiss@gmail.com>
2016-08-05 16:51:45 -0500

    stars

--- app_client/common/directives/ratingStars/ratingStars.directive.js
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -1,39 +1,20 @@
-(function () {
 
+angular.module('loc8rApp', ['mm.foundation']);
+angular.module('loc8rApp').controller('ratingCtrl', function ($scope) {
 
-angular
-   .module('loc8rApp')
-   .directive('ratingStars', ratingStars);
-
-function ratingStars () {
-	return {
-		restrict: 'EA',
-		scope: {
-			thisRating : '=rating'
-		},
-		templateUrl: '/common/directives/ratingStars/ratingStars.template.html',
-    };
- }
-
-})();
-
-
-angular.module('foundationDemoApp').controller('RatingDemoCtrl', function ($scope) {
-  $scope.rate = 7;
-  $scope.max = 10;
-  $scope.isReadonly = false;
-
-  $scope.hoveringOver = function(value) {
-    $scope.overStar = value;
-    $scope.percent = 100 * (value / $scope.max);
-  };
+ 
 
   $scope.ratingStates = [
-    {stateOn: 'fa-check-circle', stateOff: 'fa-check-circle-o'},
-    {stateOn: 'fa-star', stateOff: 'fa-start-o'},
-    {stateOn: 'fa-heart', stateOff: 'fa-ban'},
-    {stateOn: 'fa-heart'},
-    {stateOff: 'fa-power-off'}
+    {stateOn: 'fi-star', stateOff: 'fi-star'},
+    {stateOn: 'fi-star', stateOff: 'fi-star'},
+    {stateOn: 'fi-star', stateOff: 'fi-star'},
+    {stateOn: 'fi-star'},
+    {stateOff: 'fi-star'}
   ];
 });
-  
+
+
+
+
+
+
