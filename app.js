cbb77d198db09b29a45785de1220a3c72c097cbb
Michael Wiss <michael.wiss@gmail.com>
2016-09-19 18:03:26 -0500

    expose api

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 var users = require('./app_server/routes/users');
 
+
 var app = express();
 
 // view engine setup
